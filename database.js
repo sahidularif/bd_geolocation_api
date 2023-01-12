49218ac72fcf9da73acd9d403cb415676ea308ac
sahidularif <sahidularif1@gmail.com>
2023-01-13 02:07:33 +0600

    finished

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ mongoose.set('strictQuery', true);
 const DB2 = 'mongodb://localhost:27017/geolocation'
 const DB = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.or4h7.mongodb.net/geolocation?retryWrites=true&w=majority`
 module.exports = () => {
-    mongoose.connect(DB2, {
+    mongoose.connect(DB, {
         useNewUrlParser: true,
         // useCreateIndex: true,
         useUnifiedTopology: true,
